5868c103941899cac2de0326c27d6834ecd4fb75
Lekharaj A G <127710430+Lekharaj-A-G@users.noreply.github.com>
2024-02-29 13:32:16 +0000

    commited

---
+++ 01basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // After declaring this the code will be treated as the newer version of javascript
+
+// alert("hello")  // we are using nodejs not browser so this line will give error
+
+//datatypes
+
+let name = "kiran"
+let age = 12
+let isLoggedIn = false
+
+
+// number  = it has range of 2 to the power 53
+// String = usually written in "" or ''
+// boolean = returns true or false
+// null = it is standalone value
+// undefined = means the variable declared but the value is not assigned
+// bigInt = it is used to declare the big integers
+// Symbol = it is used to define the uniques of the variable , frequently used in reactjs and designing tools
+// object =  
+console.log(typeof 12);        // returns number
+console.log(typeof "kiran");   // returns String
+console.log(typeof false);     // returns boolean
+console.log(typeof null);      // returns object
+console.log(typeof undefined); // returns undefined
