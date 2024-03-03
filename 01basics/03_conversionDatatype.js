978ee746db7683ac77f8bc677e58ae16b8016def
Lekharaj A G <127710430+Lekharaj-A-G@users.noreply.github.com>
2024-03-03 10:07:29 +0000

    container commit done

---
+++ 01basics/03_conversionDatatype.js
@@ -0,0 +1,28 @@
+let score = undefined
+// console.log(score);
+// console.log(typeof score);
+
+let numberScore = Number(score)
+// console.log(numberScore);
+// console.log(typeof numberScore);
+
+// "33" => 33
+// "33abc" => NaN => Not a Number
+// null => 0
+// undefined = NaN
+
+
+let isLoggedIn = 0
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn);
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+
+// 1 = true
+// 0 = false
+// "lekharaj" = true
+// "" = false 
+
